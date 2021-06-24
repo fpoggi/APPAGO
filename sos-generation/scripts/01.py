7b463cfe805bb12053b0a16088de6b64031b44f3
fpoggi <francesco.poggi5@unibo.it>
2021-06-24 11:02:05 +0200

    update 01.py

--- sos-generation/scripts/01.py
+++ sos-generation/scripts/01.py
@@ -6,11 +6,39 @@ from sklearn.neighbors import NearestCentroid
 import numpy as np
 import math
 
+from scipy.spatial import distance
+
 from glob import glob
 import sys
   
 csv_folder = "../listening-history/csv/"
 min_msPlayed = 10000
+maxClusters = 10
+
+
+# loads csv data into a dataframe.
+# filters songs by size (length >= min_ms_played)
+# computes new columns, i.e. date, time and time-hour
+def loadData(filename, min_msPlayed):
+  df = pd.read_csv(filename,delimiter=";", encoding="UTF-8") #"ISO-8859-1")  
+  numLines = df.shape[0]
+  df["endTime"] = pd.to_datetime(df["endTime"], format="%Y-%m-%d %H:%M")
+  df = df.sort_values(by=["endTime"])
+  df_filtered = df[df["msPlayed"] >= min_msPlayed]
+  print(f"File: {filename} - Size: {df_filtered.shape[0]}/{numLines}")
+  #print("File: " + filename + " - Size: " + str(df_filtered.shape[0]) + "/" + str(numLines))
+  
+  #df_filtered["date"] = df_filtered["endTime"].apply(lambda x: x.date())
+  temp = df_filtered["endTime"].apply(lambda x: str(x.date()))
+  df_filtered.insert(loc=3,column="date",value=temp)
+  temp = df_filtered["endTime"].apply(lambda x: str(x).split(" ")[1]) #str(x.hour) + ":" + str(x.minute) )
+  df_filtered.insert(loc=4,column="time",value=temp)
+  temp = df_filtered["endTime"].apply(lambda x: str(x).split(" ")[1].split(":")[0]) #str(x.hour) + ":" + str(x.minute) )
+  df_filtered.insert(loc=5,column="time-hour",value=temp)
+  
+  return df_filtered
+
+
 
 # Section 3.1
 def computeNTNA_NTKA(df):
@@ -52,7 +80,7 @@ def songs_byHour(df, h):
   
 
 
-
+"""
 def centroid(l,M):
   c = [ ]
   # per ogni componente = colonna d M
@@ -65,54 +93,12 @@ def centroid(l,M):
     v = v / len(l)    
     c.append(v)
   return c
+"""
 
 #Section 3.3
-"""
-# prende la matrice M e calcola il miglior numero di cluster usando DB
-def best_k_means_Manuela(M):
-  # primo cluster contenente tutto l'insieme di elementi
-  m = len(M)
-  bDB = 100
-    
-  #costruisco gli altri cluster - voglio che si possa fare in maniera incrementale
-  # k e' l'effettivo numero di cluster: il primo viene costruito prima del while
-  k = 2
-    
-  while k < 10:
-        
-    C = k_means(M,k)
-    #cl = indici_cluster(C) # non va con k-means perche' C e' come in FPF
-    cl = C[1] # indici dei cluster
-    cc = centroid_list(cl,M)
-    db = DB(cc,cl,M)
-    # per forzarlo a fare 3 o 4 cluster
-    #if (len(cl) == 2 or len(cl) == 3):
-    #    db = 10
-    if db < bDB:
-      bDB = db
-      bcl = cl
-    print "number of cluster = ", len(cl)
-    print "DB = ", db
-    k = k + 1
-        
-  print "best number of cluster = ", len(bcl)
-  print "bestDB = ", bDB
-  # restituisce il numero di cluster e le liste con i numeri di riga degli elementi in ogni cluster (centro incluso)
-  return (len(bcl),bcl)
-"""
-
-# Funzione che calcola la distanza euclidea tra due vettori
-def EDist(X,Y):
-  d = 0
-  if len(X) == len(Y):
-    for i in [0,len(X) -1]:
-      d = d + (X[i] - Y[i])**2
-  return math.sqrt(d)
-    
-  
-# calocla l'indice Davis-Boudin. Input cc = centroidi; cl = clustering come indici di riga di M;
+# calcola l'indice Davis-Boudin. Input cc = centroidi; cl = clustering come indici di riga di M;
 # M = matrice dati. Restituisce indice numerico
-def DB(cc, cl, M):
+def DaviesBouldinIndex(cc, cl, M):
   S = [ ]
   # per ogni cluster i calcolo i valori S
   for i in range(0,len(cl)):
@@ -120,12 +106,11 @@ def DB(cc, cl, M):
     c = cl[i]
     # distanze di ogni elemento j dal centroide
     for j in range(0,len(c)):
-      s = s + EDist(cc[i],M[c[j]])
+      s = s + distance.euclidean(cc[i],M[c[j]])
     s = s / len(c)
     S.append(s)
 
   # calcolo D_i per ogni  cluster 
-
   D = [ ]
     
   # per ogni coppia di cluster (i,j) con (i != j) calcolo i R_i,j e prendo il max
@@ -136,7 +121,7 @@ def DB(cc, cl, M):
     # per ogni altro cluster j
     for j in range (0,len(cl)):
       if (i != j):
-        r = (S[i] + S[j]) / EDist(cc[i],cc[j])
+        r = (S[i] + S[j]) / distance.euclidean(cc[i],cc[j])
         if r > d:
           d = r
       D.append(d)
@@ -149,39 +134,27 @@ def DB(cc, cl, M):
   DB = DB / len(D)
   return DB
 
-def best_k_means(df):
+
+def best_k_means(df, maxClusters=10):
   # primo cluster contenente tutto l'insieme di elementi
-  #m = len(M)
   bDB = 100
     
   #costruisco gli altri cluster - voglio che si possa fare in maniera incrementale
   # k e' l'effettivo numero di cluster: il primo viene costruito prima del while
-  maxClusters = 10
-    
-  df_features = df[["Acousticness", "Danceability", "Energy", "Instrumentalness", "Key", "Liveness", "Loudeness", "Mode", "Speechiness", "Tempo", "Time_signature", "Valence"]]
-  print(df_features.head().values.tolist())
+  
+  #print(df_features.head().values.tolist())
 
   for k in range(2,maxClusters+1):
         
-    """
-    C = k_means(M,k)
-    #cl = indici_cluster(C) # non va con k-means perche' C e' come in FPF
-    cl = C[1] # indici dei cluster
-    cc = centroid_list(cl,M)
-    """
-    
-    #df_features = pd.DataFrame([[1,1],[2,2],[2,1],[1,2],[4,4],[5,5],[4,5],[5,4]])
-    kmeans = KMeans(n_clusters=k, random_state=1).fit(df_features)
+    kmeans = KMeans(n_clusters=k, random_state=1).fit(df)
   
     #TODO: DELETE
     #cl=kmeans.labels_.tolist()
     K = kmeans.labels_
-    #print K
     
     clf = NearestCentroid()
-    clf.fit(df_features, K)
+    clf.fit(df, K)
     centroids = clf.centroids_
-    #print(centroids)
 
     # costruisco il clustering come serve a me
     cl = [ ] 
@@ -190,34 +163,151 @@ def best_k_means(df):
     for i in range(0,len(K)):
       cl[K[i]].append(i)
     
-    db = DB(centroids, cl, df_features.values.tolist())
+    db = DaviesBouldinIndex(centroids, cl, df.values.tolist())
     if db < bDB:
       bDB = db
       bcl = cl
+      bLabels = K
     print("number of cluster = ", len(cl))
     print("DB = ", db)
         
   print("best number of cluster = ", len(bcl))
   print("bestDB = ", bDB)
   # restituisce il numero di cluster e le liste con i numeri di riga degli elementi in ogni cluster (centro incluso)
-  return (len(bcl),bcl)
-  """
-    db = DB(cc,cl,M)
-    # per forzarlo a fare 3 o 4 cluster
-    #if (len(cl) == 2 or len(cl) == 3):
-    #    db = 10
-    if db < bDB:
-      bDB = db
-      bcl = cl
-    print "number of cluster = ", len(cl)
-    print "DB = ", db
-        
-  print "best number of cluster = ", len(bcl)
-  print "bestDB = ", bDB
-  # restituisce il numero di cluster e le liste con i numeri di riga degli elementi in ogni cluster (centro incluso)
-  return (len(bcl),bcl)
-  """
+  return {"best-length": len(bcl), "best-index-list": bcl, "best-centroids": centroids, "best-labels": bLabels}
   
+
+def linearHeuristic(df, best_Kmeans):
+  
+  df_copy = df.copy(deep=True)
+
+  kLength = best_Kmeans["best-length"]
+  #kIndexes = best_Kmeans["best-index-list"]
+  kCentroids = best_Kmeans["best-centroids"]
+  kLabels = best_Kmeans["best-labels"]
+
+  # add cluster labels to the DF
+  df_copy.insert(loc=len(df_copy.columns),column="kLabel",value=kLabels)
+  
+  # add coordinates of centroids to the DF
+  centroids = df_copy["kLabel"].apply(lambda x: kCentroids[x])
+  df_copy.insert(loc=len(df_copy.columns),column="kCentroid",value=centroids)
+  
+  # add cluster labels to the DF
+  coordinates = list()
+  for index, row in df_copy.iterrows():
+    coordinates.append([row["Acousticness"],row["Danceability"],row["Energy"],row["Instrumentalness"],row["Key"],row["Liveness"],row["Loudeness"],row["Mode"],row["Speechiness"],row["Tempo"],row["Time_signature"],row["Valence"]])
+  df_copy.insert(loc=len(df_copy.columns),column="kCoordinates",value=coordinates)
+  
+  # add cluster labels to the DF
+  distances = list()
+  for index, row in df_copy.iterrows():
+    distances.append(distance.euclidean(row["kCoordinates"],row["kCentroid"]))
+  df_copy.insert(loc=len(df_copy.columns),column="kDistance",value=distances)
+  
+  df_copy.sort_values(by=["kLabel","kDistance"], inplace=True)
+  
+  # choose 4 songs in each cluster
+  songsDFs = list()
+  for i in range(0,kLength):
+    df_slice = df_copy[df_copy["kLabel"] == i]
+    numPoints = df_slice.shape[0]
+    songsDFs.append(df_slice.iloc[[0, numPoints // 3 - 1, (numPoints // 3) * 2 - 1, numPoints - 1]])
+    
+  return songsDFs #{"songsByCluster": songsDFs, "centroids": kCentroids}
+
+
+def sphereHeuristic(df, best_Kmeans):
+  
+  df_copy = df.copy(deep=True)
+
+  kLength = best_Kmeans["best-length"]
+  #kIndexes = best_Kmeans["best-index-list"]
+  kCentroids = best_Kmeans["best-centroids"]
+  kLabels = best_Kmeans["best-labels"]
+
+  # add cluster labels to the DF
+  df_copy.insert(loc=len(df_copy.columns),column="kLabel",value=kLabels)
+  
+  # add coordinates of centroids to the DF
+  centroids = df_copy["kLabel"].apply(lambda x: kCentroids[x])
+  df_copy.insert(loc=len(df_copy.columns),column="kCentroid",value=centroids)
+  
+  # add cluster labels to the DF
+  coordinates = list()
+  for index, row in df_copy.iterrows():
+    coordinates.append([row["Acousticness"],row["Danceability"],row["Energy"],row["Instrumentalness"],row["Key"],row["Liveness"],row["Loudeness"],row["Mode"],row["Speechiness"],row["Tempo"],row["Time_signature"],row["Valence"]])
+  df_copy.insert(loc=len(df_copy.columns),column="kCoordinates",value=coordinates)
+  
+  # add cluster labels to the DF
+  distances = list()
+  for index, row in df_copy.iterrows():
+    distances.append(distance.euclidean(row["kCoordinates"],row["kCentroid"]))
+  df_copy.insert(loc=len(df_copy.columns),column="kDistance",value=distances)
+  
+  df_copy.sort_values(by=["kLabel","kDistance"], inplace=True)
+  
+  # choose 4 songs in each cluster
+  songsDFs = list()
+  for i in range(0,kLength):
+    #df_slice = df_copy[df_copy["kLabel"] == i]
+    #numPoints = df_slice.shape[0]
+    #songsDFs.append(df_slice.iloc[[0, numPoints // 3 - 1, (numPoints // 3) * 2 - 1, numPoints - 1]])
+    df_slice = df_copy[df_copy["kLabel"] == i]
+    print(df_slice)
+    print(df_slice.iloc[df_slice.shape[0] - 1])
+    print(df_slice.iloc[df_slice.shape[0] - 1]["kDistance"])
+    sys.exit()
+    
+  return songsDFs #{"songsByCluster": songsDFs, "centroids": kCentroids}
+
+
+
+
+
+    
+contents = glob(f"{csv_folder}*.csv")
+#contents = glob(csv_folder + "*.csv")
+
+contents.sort()
+for filename in contents:
+  print (filename)
+  df = loadData(filename, min_msPlayed)
+  
+  #df.to_csv("test.csv")
+  #print(df.groupby(["date","time-hour"]).count())
+  
+  # 3.1
+  ntna_ntka = computeNTNA_NTKA(df)
+  
+  # 3.2
+  df_h = songs_byHour(df, "09")
+  
+  #3.3
+  #K-MEANS
+  #print(df_filtered.columns.values)
+  #test_kmeans(df_filtered)
+  df_h_feat = df_h[["Acousticness", "Danceability", "Energy", "Instrumentalness", "Key", "Liveness", "Loudeness", "Mode", "Speechiness", "Tempo", "Time_signature", "Valence"]]
+  best_Kmeans = best_k_means(df_h_feat, maxClusters)
+  #linear_kMeans = linearHeuristic(df_h, best_Kmeans)
+  sphere_lMeans = sphereHeuristic(df_h, best_Kmeans)
+  #print(res)
+  
+  sys.exit()
+  
+  #for index, row in df_filtered.iterrows():
+  #  print(row["endTime"])
+  #  print(row["endTime"].date())
+  #  print(row["endTime"].hour)
+  
+  #break
+
+
+
+
+
+
+'''
 def test_kmeans(df):
   k=2
   temp = df[["Acousticness", "Danceability", "Energy", "Instrumentalness", "Key", "Liveness", "Loudeness", "Mode", "Speechiness", "Tempo", "Time_signature", "Valence"]]
@@ -248,57 +338,7 @@ def test_kmeans(df):
   #return cc
   print(cc)
   """
-
-
-contents = glob(f"{csv_folder}*.csv")
-#contents = glob(csv_folder + "*.csv")
-
-contents.sort()
-for filename in contents:
-  print (filename)
-  df = pd.read_csv(filename,delimiter=";", encoding="UTF-8") #"ISO-8859-1")  
-  numLines = df.shape[0]
-  df["endTime"] = pd.to_datetime(df["endTime"], format="%Y-%m-%d %H:%M")
-  df = df.sort_values(by=["endTime"])
-  df_filtered = df[df["msPlayed"] > min_msPlayed]
-  print(f"File: {filename} - Size: {df_filtered.shape[0]}/{numLines}")
-  #print("File: " + filename + " - Size: " + str(df_filtered.shape[0]) + "/" + str(numLines))
-  
-  #df_filtered["date"] = df_filtered["endTime"].apply(lambda x: x.date())
-  temp = df_filtered["endTime"].apply(lambda x: str(x.date()))
-  df_filtered.insert(loc=3,column="date",value=temp)
-  temp = df_filtered["endTime"].apply(lambda x: str(x).split(" ")[1]) #str(x.hour) + ":" + str(x.minute) )
-  df_filtered.insert(loc=4,column="time",value=temp)
-  temp = df_filtered["endTime"].apply(lambda x: str(x).split(" ")[1].split(":")[0]) #str(x.hour) + ":" + str(x.minute) )
-  df_filtered.insert(loc=5,column="time-hour",value=temp)
-  #df_filtered["time-hour"]
-  
-  #print(df_filtered[["date","trackName"]].head(10))
-  #print(df_filtered.groupby(["date","time-hour"]).count())
-
-  # 3.1
-  #ntna_ntka = computeNTNA_NTKA(df_filtered)
-  #print(ntna_ntka)
-
-  # 3.2
-  #print(songs_byHour(df_filtered, "09"))
-  #print(df_filtered.info())
-  #sys.exit()
-  
-  #3.3
-  #K-MEANS
-  #print(df_filtered.columns.values)
-  #test_kmeans(df_filtered)
-  best_k_means(df_filtered)
-  sys.exit()
-  
-  #for index, row in df_filtered.iterrows():
-  #  print(row["endTime"])
-  #  print(row["endTime"].date())
-  #  print(row["endTime"].hour)
-  
-  #break
-
+'''
 
 """
 Dig Down (Acoustic Gospel Version)
