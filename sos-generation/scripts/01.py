923e7d933b8c314f42a95b3a9c3f6ab82913719f
fpoggi <francesco.poggi5@unibo.it>
2021-06-28 15:54:20 +0200

    update 01.py

--- sos-generation/scripts/01.py
+++ sos-generation/scripts/01.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import NearestCentroid
 #sklearn.neighbors.NearestCentroid
 import numpy as np
 import math
+from scipy.special import gammainc
 
 from scipy.spatial import distance
 
@@ -71,12 +72,13 @@ def computeNTNA_NTKA(df):
 
 # Section 3.2
 def songs_byHour(df, h):
+  df_copy = df.copy(deep=True)
   if type(h) == str and len(h) != 2:
     print("ERROR in songs_byHour()")
     sys.exit()
   if type(h) == int:
     h = "{:02d}".format(h)
-  return df[df["time-hour"] == h]
+  return df_copy[df_copy["time-hour"] == h]
   
 
 
@@ -102,6 +104,7 @@ def DaviesBouldinIndex(cc, cl, M):
   S = [ ]
   # per ogni cluster i calcolo i valori S
   for i in range(0,len(cl)):
+    
     s = 0
     c = cl[i]
     # distanze di ogni elemento j dal centroide
@@ -135,7 +138,7 @@ def DaviesBouldinIndex(cc, cl, M):
   return DB
 
 
-def best_k_means(df, maxClusters=10):
+def best_k_means(df, maxClusters=10, approach="exclude_K_less_4_songs"):
   # primo cluster contenente tutto l'insieme di elementi
   bDB = 100
     
@@ -152,10 +155,24 @@ def best_k_means(df, maxClusters=10):
     #cl=kmeans.labels_.tolist()
     K = kmeans.labels_
     
+    
     clf = NearestCentroid()
     clf.fit(df, K)
     centroids = clf.centroids_
 
+    
+    
+    
+    
+    # add cluster labels to the DF
+    df_temp = df.copy(deep=True)
+    df_temp.insert(loc=len(df_temp.columns),column="kLabel",value=K)
+    #df_count = df_temp.groupby(df_temp["kLabel"]).count()
+    #sys.exit()
+    
+    
+    
+    
     # costruisco il clustering come serve a me
     cl = [ ] 
     for i in range(0,k):
@@ -163,16 +180,62 @@ def best_k_means(df, maxClusters=10):
     for i in range(0,len(K)):
       cl[K[i]].append(i)
     
+
+    # manage clusters with less than 4 songs
+    clustersToExclude = list()
+    for i in range(1,k):
+      numSongs = len(cl[i])
+      if numSongs < 4:
+        clustersToExclude.append(i)
+    
+    if len(clustersToExclude) > 0:
+      # 
+      if approach == "exclude_K_less_4_songs":
+      #cerco cluster con minor numero di songs (per controllare che #songs nei cluster sia >= 4 - per passi seguenti)
+      #minSongs = len(cl[0])
+      #for i in range(1,k):
+      #  numSongs = len(cl[i])
+      #  if numSongs < minSongs:
+      #    minSongs = numSongs
+      #if  minSongs < 4:
+      #  continue
+        continue
+
+      elif approach == "exclude_cluster_less_4_songs":
+        print(clustersToExclude)
+        print(df_temp.shape[0])
+        df_temp2 = df_temp[~df_temp["kLabel"].isin(clustersToExclude)]
+        df = df_temp2.drop(columns=["kLabel"])
+        print(df)
+        
+        print(centroids)
+        centroids = [centroids[i] for i in range(0,len(centroids)) if i not in clustersToExclude]
+        print(centroids)
+        
+        K = pd.Series(df_temp2["kLabel"]).values
+        
+        # costruisco il clustering come serve a me
+        cl = [ ] 
+        for i in range(0,k):
+          cl.append([ ])
+        for i in range(0,len(K)):
+          cl[K[i]].append(i)
+        
+
+    
+    
+    
     db = DaviesBouldinIndex(centroids, cl, df.values.tolist())
     if db < bDB:
       bDB = db
       bcl = cl
       bLabels = K
-    print("number of cluster = ", len(cl))
+    print("number of cluster = ", len(cl))#, " - minSongs = ", minSongs)
     print("DB = ", db)
         
   print("best number of cluster = ", len(bcl))
   print("bestDB = ", bDB)
+  
   # restituisce il numero di cluster e le liste con i numeri di riga degli elementi in ogni cluster (centro incluso)
   return {"best-length": len(bcl), "best-index-list": bcl, "best-centroids": centroids, "best-labels": bLabels}
   
@@ -217,11 +280,45 @@ def linearHeuristic(df, best_Kmeans):
   return songsDFs #{"songsByCluster": songsDFs, "centroids": kCentroids}
 
 
-def sphereHeuristic(df, best_Kmeans):
+
+
+
+#n_per_sphere e' il numero di punti che si vogliono generare
+
+def sample(center,radius,n_per_sphere):
+  r = radius
+  ndim = center.size
+  x = np.random.normal(size=(n_per_sphere, ndim))
+  ssq = np.sum(x**2,axis=1)
+  fr = r*gammainc(ndim/2,ssq/2)**(1/ndim)/np.sqrt(ssq)
+  frtiled = np.tile(fr.reshape(n_per_sphere,1),(1,ndim))
+  p = center + np.multiply(x,frtiled)
+  return p
+
+"""
+#calcola il raggio (come max delle distanze dal centroide)
+def raggioMAX(cl,c,M):
+  maxD = EDist(c,M[cl[0]]) #inizializzo la distanza massima a quella con il centro
+  farthest = cl[0] #inizializzo il piu' lontano al centro 
+  # distanze di ogni elemento j dal centroide
+  for j in range(1,len(cl)):
+    dist = EDist(c,M[cl[j]])
+    if dist > maxD:
+      maxD = dist
+      farthest = cl[j]
+  return [maxD, farthest]
+"""
+
+
+def getPointAllowedFeatureValues():
+  print()
+  
+  
+def sphereHeuristic(df, best_Kmeans, num_track = 50):
   
   df_copy = df.copy(deep=True)
 
-  kLength = best_Kmeans["best-length"]
+  kLength = best_Kmeans["best-length"] # numero di cluster
   #kIndexes = best_Kmeans["best-index-list"]
   kCentroids = best_Kmeans["best-centroids"]
   kLabels = best_Kmeans["best-labels"]
@@ -246,20 +343,75 @@ def sphereHeuristic(df, best_Kmeans):
   df_copy.insert(loc=len(df_copy.columns),column="kDistance",value=distances)
   
   df_copy.sort_values(by=["kLabel","kDistance"], inplace=True)
-  
-  # choose 4 songs in each cluster
-  songsDFs = list()
+ 
   for i in range(0,kLength):
-    #df_slice = df_copy[df_copy["kLabel"] == i]
-    #numPoints = df_slice.shape[0]
-    #songsDFs.append(df_slice.iloc[[0, numPoints // 3 - 1, (numPoints // 3) * 2 - 1, numPoints - 1]])
     df_slice = df_copy[df_copy["kLabel"] == i]
-    print(df_slice)
-    print(df_slice.iloc[df_slice.shape[0] - 1])
-    print(df_slice.iloc[df_slice.shape[0] - 1]["kDistance"])
-    sys.exit()
     
-  return songsDFs #{"songsByCluster": songsDFs, "centroids": kCentroids}
+    #RandC = raggioMAX(CL[1][i],centroidi[i],M)
+    #radius = RandC[0]
+    farthestPoint = df_slice.iloc[df_slice.shape[0]-1]
+    radius = farthestPoint["kDistance"]
+    RandC = [radius, farthestPoint]
+    
+    #genera 5 punti a caso nella sfera con centro il centroide e raggio quello che e' --> f matrice con un punto per riga
+    #center = np.array(centroidi[i])
+    #p = sample(center,radius,100)
+    #f = np.array(p).tolist()
+    center = np.array(farthestPoint["kCentroid"])
+    p = sample(center,radius,100)
+    f = np.array(p).tolist()
+    
+    ##################################################
+    # TODO FPOGGI: questo va portato fuori dal ciclo #
+    ##################################################
+    #controllo che i valori siano nei range, altrimenti li modifico accordingly
+    # conto quanti punti diversi trovo con track_id diversi --> np fino a 5
+    #num_track = 50
+    pp = 0
+    totp = int(num_track/(4*kLength)) + 1 
+    # tengo lista track_id
+    tr = [ ]
+    ##################################################
+    # END TODO FPOGGI: questo va portato fuori dal ciclo #
+    ##################################################
+    
+    j = 0
+    print ("ciclo i" , i)
+    
+    minDistTrackIds = list()
+    minDistSongs = list()
+    # ciclo da ripetere totp ()volte
+    #while (j < len(f) and pp <= totp ):
+    while (i==4 and j<len(f) and len(minDistSongs)<4):
+    #for j in range (0,4):
+      currRandomPoint = f[j]
+      print("\t",i,j)
+      #print(currRandomPoint)
+      print("\t",df_slice["kCoordinates"])#.shape[0])
+      
+      #1. minD, closest (indice della canz. nel cluster) e seed (la canzone del cluster) = del punto del cluster più vicino al j-esimo casuale
+      #2. newP = punto con coordinate nel range previsto delle audio feature, partendo da j-esimo punto casuale
+      minDist = -1
+      for index, row in df_slice.iterrows():
+        #print(row)
+        #sys.exit()
+        dist = distance.euclidean(currRandomPoint, row["kCoordinates"])
+        if dist < minDist or minDist == -1:
+          minDist = dist
+          minDistSong = row
+      
+      if minDistSong["TrackID"] not in minDistTrackIds:
+        minDistTrackIds.append(minDistSong["TrackID"])
+        minDistSongs.append({"minDist": minDist, "minDistSong": minDistSong})
+      else:
+        print("Already selected: skip.")
+      j += 1
+    
+    print(len(minDistSongs))
+    #sys.exit()  
+      
+                
+  return "ciao"
 
 
 
@@ -281,16 +433,23 @@ for filename in contents:
   ntna_ntka = computeNTNA_NTKA(df)
   
   # 3.2
-  df_h = songs_byHour(df, "09")
+  #df_h = songs_byHour(df, "09")
+  df_h = df
+  
+  # Remove duplicate songs
+  df_h.drop_duplicates(subset ="TrackID",
+                     keep = False, inplace = True)
   
   #3.3
   #K-MEANS
   #print(df_filtered.columns.values)
   #test_kmeans(df_filtered)
   df_h_feat = df_h[["Acousticness", "Danceability", "Energy", "Instrumentalness", "Key", "Liveness", "Loudeness", "Mode", "Speechiness", "Tempo", "Time_signature", "Valence"]]
-  best_Kmeans = best_k_means(df_h_feat, maxClusters)
+  
+  best_Kmeans = best_k_means(df_h_feat, maxClusters, "exclude_cluster_less_4_songs")
+  sys.exit()
   #linear_kMeans = linearHeuristic(df_h, best_Kmeans)
-  sphere_lMeans = sphereHeuristic(df_h, best_Kmeans)
+  sphere_kMeans = sphereHeuristic(df_h, best_Kmeans)
   #print(res)
   
   sys.exit()
